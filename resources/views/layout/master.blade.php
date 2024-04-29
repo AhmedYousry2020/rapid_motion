e5ba339c85c919aa9be46b6e98abfa02bc72eba3
Ahmed Yousry <63677504+AhmedYousry2020@users.noreply.github.com>
2024-04-29 13:36:37 +0300

    updates in clients

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -5,8 +5,8 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Azzle - AI Technology & Startup HTML Template</title>
-    <meta name="description" content="AIMass Tailwind based SASS Template" />
+    <title>Rapid Motion</title>
+    <meta name="description" content="Rapid Motion" />
 
     <!-- Favicon  -->
     <link rel="icon" href="assets/img/favicon.png" />
